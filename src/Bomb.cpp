c1200b0751b25ce3523ec0d0ef079bf3abb23b3a
belinhacbr <belinhacbr@gmail.com>
2015-11-27 01:30:56 -0200

    Refactor fire animation

--- src/Bomb.cpp
+++ src/Bomb.cpp
@@ -93,7 +93,6 @@ void Bomb :: moveAnimation(){
     }
     switch(state){
         case SET:
-            //action();
             frame = (frame+1)%6;
             dx0 = frame/2*GRID_SIZE;
             dy0 = 3*GRID_SIZE;
@@ -104,254 +103,42 @@ void Bomb :: moveAnimation(){
             if(frame==8){
                 state = ASH;
             }else{
-                displayImage(fire_sprite, sx, sy);
+                dx0 = frame/2*GRID_SIZE;
+                displaySpriteImage(fire_sprite, sx, sy, dx0, 3*GRID_SIZE, GRID_SIZE, colorkey);
                 if(x-range>=0 && map.get(y,x-range)==FIRE){ //Left
-                    displayImage(fire_sprite, sx-range*GRID_SIZE, sy);
+                    displaySpriteImage(fire_sprite, sx-range*GRID_SIZE, sy, dx0, 5*GRID_SIZE, GRID_SIZE, colorkey);
                 }
                 for(int i = 1; i<range; i++){
                     if((x-i)>=0 && map.get(y,x-i)==FIRE){ //Left middle
-                        displayImage(fire_sprite, sx-i*GRID_SIZE, sy);
+                        displaySpriteImage(fire_sprite, sx-i*GRID_SIZE, sy, dx0, 1*GRID_SIZE, GRID_SIZE, colorkey);
                     }
                 }
                 if(y-range>=0 && map.get(y-range,x)==FIRE){ //Up
-                    displayImage(fire_sprite, sx, sy-range*GRID_SIZE);
+                    displaySpriteImage(fire_sprite, sx, sy-range*GRID_SIZE, dx0, 0, GRID_SIZE, colorkey);
                 }
                 for(int i = 1; i<range; i++){
                     if((y-i)>=0 && map.get(y-i,x)==FIRE){ //Up Middle
-                        displayImage(fire_sprite, sx, sy-i*GRID_SIZE);
+                        displaySpriteImage(fire_sprite, sx, sy-i*GRID_SIZE, dx0, 4*GRID_SIZE, GRID_SIZE, colorkey);
                     }
                 }
                 if(x+range<MAX_X && map.get(y,x+range)==FIRE){ //Right
-                    displayImage(fire_sprite, sx+range*GRID_SIZE, sy);
+                    displaySpriteImage(fire_sprite, sx+range*GRID_SIZE, sy, dx0, 6*GRID_SIZE, GRID_SIZE, colorkey);
                 }
                 for(int i = 1; i<range; i++){
                     if((x+i)<MAX_X && map.get(y,x+i)==FIRE){ //Right Middle
-                        displayImage(fire_sprite, sx+i*GRID_SIZE, sy);
+                        displaySpriteImage(fire_sprite, sx+i*GRID_SIZE, sy, dx0, 2*GRID_SIZE, GRID_SIZE, colorkey);
                     }
                 }
                 if(y+range<MAX_Y && map.get(y+range,x)==FIRE){ //Down
-                    displayImage(fire_sprite, sx, sy+range*GRID_SIZE);
+                    displaySpriteImage(fire_sprite, sx, sy+range*GRID_SIZE, dx0, 7*GRID_SIZE, GRID_SIZE, colorkey);
                 }
                 for(int i = 1; i<range; i++){
                     if((y+i)<MAX_Y && map.get(y+i,x)==FIRE){ //Down Middle
-                        displayImage(fire_sprite, sx, sy+i*GRID_SIZE);
+                        displaySpriteImage(fire_sprite, sx, sy+i*GRID_SIZE, dx0, 4*GRID_SIZE, GRID_SIZE, colorkey);
                     }
                 }
             }
             break;
     }
-    /*switch(frame){
-        case 0:
-            if(state==SET){
-                //displaySpriteImage(enemy_sprite, sx, sy, dx0*sprite_size, dy0, sprite_size, colorkey);
-            }else if(state==EXPLODE){
-                displayImage(fireCenterImage1, sx, sy);
-                if(x-range>=0 && map.get(y,x-range)==FIRE)
-                    displayImage(fireLeftImage1, sx-range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x-i)>=0 && map.get(y,x-i)==FIRE)
-                        displayImage(fireLeftMiddleImage1, sx-i*32, sy);
-                if(y-range>=0 && map.get(y-range,x)==FIRE)
-                    displayImage(fireUpImage1, sx, sy-range*32);
-                for(int i = 1; i<range; i++)
-                    if((y-i)>=0 && map.get(y-i,x)==FIRE)
-                        displayImage(fireUpMiddleImage1, sx, sy-i*32);
-                if(x+range<13 && map.get(y,x+range)==FIRE)
-                    displayImage(fireRightImage1, sx+range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x+i)<13 && map.get(y,x+i)==FIRE)
-                        displayImage(fireRightMiddleImage1, sx+i*32, sy);
-                if(y+range<11 && map.get(y+range,x)==FIRE)
-                    displayImage(fireDownImage1, sx, sy+range*32);
-                for(int i = 1; i<range; i++)
-                    if((y+i)<11 && map.get(y+i,x)==FIRE)
-                        displayImage(fireDownMiddleImage1, sx, sy+i*32);
-            }
-            break;
-        case 1:
-            if(state==SET){
-                //displaySpriteImage(enemy_sprite, sx, sy, dx0*sprite_size, dy0, sprite_size, colorkey);
-            }else if(state==EXPLODE){
-                displayImage(fireCenterImage2, sx, sy);
-                if(x-range>=0 && map.get(y,x-range)==FIRE)
-                    displayImage(fireLeftImage2, sx-range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x-i)>=0 && map.get(y,x-i)==FIRE)
-                        displayImage(fireLeftMiddleImage2, sx-i*32, sy);
-                if(y-range>=0 && map.get(y-range,x)==FIRE)
-                    displayImage(fireUpImage2, sx, sy-range*32);
-                for(int i = 1; i<range; i++)
-                    if((y-i)>=0 && map.get(y-i,x)==FIRE)
-                        displayImage(fireUpMiddleImage2, sx, sy-i*32);
-                if(x+range<13 && map.get(y,x+range)==FIRE)
-                    displayImage(fireRightImage2, sx+range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x+i)<13 && map.get(y,x+i)==FIRE)
-                        displayImage(fireRightMiddleImage2, sx+i*32, sy);
-                if(y+range<11 && map.get(y+range,x)==FIRE)
-                    displayImage(fireDownImage2, sx, sy+range*32);
-                for(int i = 1; i<range; i++)
-                    if((y+i)<11 && map.get(y+i,x)==FIRE)
-                        displayImage(fireDownMiddleImage2, sx, sy+i*32);
-            }
-            break;
-        case 2:
-            if(state==SET){
-                //displaySpriteImage(enemy_sprite, sx, sy, dx0*sprite_size, dy0, sprite_size, colorkey);
-            }else if(state==EXPLODE){
-                displayImage(fireCenterImage3, sx, sy);
-                if(x-range>=0 && map.get(y,x-range)==FIRE)
-                    displayImage(fireLeftImage3, sx-range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x-i)>=0 && map.get(y,x-i)==FIRE)
-                        displayImage(fireLeftMiddleImage3, sx-i*32, sy);
-                if(y-range>=0 && map.get(y-range,x)==FIRE)
-                    displayImage(fireUpImage3, sx, sy-range*32);
-                for(int i = 1; i<range; i++)
-                    if((y-i)>=0 && map.get(y-i,x)==FIRE)
-                        displayImage(fireUpMiddleImage3, sx, sy-i*32);
-                if(x+range<13 && map.get(y,x+range)==FIRE)
-                    displayImage(fireRightImage3, sx+range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x+i)<13 && map.get(y,x+i)==FIRE)
-                        displayImage(fireRightMiddleImage3, sx+i*32, sy);
-                if(y+range<11 && map.get(y+range,x)==FIRE)
-                    displayImage(fireDownImage3, sx, sy+range*32);
-                for(int i = 1; i<range; i++)
-                    if((y+i)<11 && map.get(y+i,x)==FIRE)
-                        displayImage(fireDownMiddleImage3, sx, sy+i*32);
-            }
-            break;
-        case 3:
-            if(state==SET){
-                //displaySpriteImage(enemy_sprite, sx, sy, dx0*sprite_size, dy0, sprite_size, colorkey);
-            }else if(state==EXPLODE){
-               displayImage(fireCenterImage4, sx, sy);
-                if(x-range>=0 && map.get(y,x-range)==FIRE)
-                    displayImage(fireLeftImage4, sx-range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x-i)>=0 && map.get(y,x-i)==FIRE)
-                        displayImage(fireLeftMiddleImage4, sx-i*32, sy);
-                if(y-range>=0 && map.get(y-range,x)==FIRE)
-                    displayImage(fireUpImage4, sx, sy-range*32);
-                for(int i = 1; i<range; i++)
-                    if((y-i)>=0 && map.get(y-i,x)==FIRE)
-                        displayImage(fireUpMiddleImage4, sx, sy-i*32);
-                if(x+range<13 && map.get(y,x+range)==FIRE)
-                    displayImage(fireRightImage4, sx+range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x+i)<13 && map.get(y,x+i)==FIRE)
-                        displayImage(fireRightMiddleImage4, sx+i*32, sy);
-                if(y+range<11 && map.get(y+range,x)==FIRE)
-                    displayImage(fireDownImage4, sx, sy+range*32);
-                for(int i = 1; i<range; i++)
-                    if((y+i)<11 && map.get(y+i,x)==FIRE)
-                        displayImage(fireDownMiddleImage4, sx, sy+i*32);
-            }
-            break;
-        case 4:
-            if(state==SET){
-                //displaySpriteImage(enemy_sprite, sx, sy, dx0*sprite_size, dy0, sprite_size, colorkey);
-            }else if(state==EXPLODE){
-               displayImage(fireCenterImage4, sx, sy);
-                if(x-range>=0 && map.get(y,x-range)==FIRE)
-                    displayImage(fireLeftImage4, sx-range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x-i)>=0 && map.get(y,x-i)==FIRE)
-                        displayImage(fireLeftMiddleImage4, sx-i*32, sy);
-                if(y-range>=0 && map.get(y-range,x)==FIRE)
-                    displayImage(fireUpImage4, sx, sy-range*32);
-                for(int i = 1; i<range; i++)
-                    if((y-i)>=0 && map.get(y-i,x)==FIRE)
-                        displayImage(fireUpMiddleImage4, sx, sy-i*32);
-                if(x+range<13 && map.get(y,x+range)==FIRE)
-                    displayImage(fireRightImage4, sx+range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x+i)<13 && map.get(y,x+i)==FIRE)
-                        displayImage(fireRightMiddleImage4, sx+i*32, sy);
-                if(y+range<11 && map.get(y+range,x)==FIRE)
-                    displayImage(fireDownImage4, sx, sy+range*32);
-                for(int i = 1; i<range; i++)
-                    if((y+i)<11 && map.get(y+i,x)==FIRE)
-                        displayImage(fireDownMiddleImage4, sx, sy+i*32);
-            }
-            break;
-        case 5:
-            if(state==SET){
-                //displaySpriteImage(enemy_sprite, sx, sy, dx0*sprite_size, dy0, sprite_size, colorkey);
-            }else if(state==EXPLODE){
-                displayImage(fireCenterImage3, sx, sy);
-                if(x-range>=0 && map.get(y,x-range)==FIRE)
-                    displayImage(fireLeftImage3, sx-range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x-i)>=0 && map.get(y,x-i)==FIRE)
-                        displayImage(fireLeftMiddleImage3, sx-i*32, sy);
-                if(y-range>=0 && map.get(y-range,x)==FIRE)
-                    displayImage(fireUpImage3, sx, sy-range*32);
-                for(int i = 1; i<range; i++)
-                    if((y-i)>=0 && map.get(y-i,x)==FIRE)
-                        displayImage(fireUpMiddleImage3, sx, sy-i*32);
-                if(x+range<13 && map.get(y,x+range)==FIRE)
-                    displayImage(fireRightImage3, sx+range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x+i)<13 && map.get(y,x+i)==FIRE)
-                        displayImage(fireRightMiddleImage3, sx+i*32, sy);
-                if(y+range<11 && map.get(y+range,x)==FIRE)
-                    displayImage(fireDownImage3, sx, sy+range*32);
-                for(int i = 1; i<range; i++)
-                    if((y+i)<11 && map.get(y+i,x)==FIRE)
-                        displayImage(fireDownMiddleImage3, sx, sy+i*32);
-            }
-            break;
-        case 6:
-            if(state==EXPLODE){
-                displayImage(fireCenterImage2, sx, sy);
-                if(x-range>=0 && map.get(y,x-range)==FIRE)
-                    displayImage(fireLeftImage2, sx-range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x-i)>=0 && map.get(y,x-i)==FIRE)
-                        displayImage(fireLeftMiddleImage2, sx-i*32, sy);
-                if(y-range>=0 && map.get(y-range,x)==FIRE)
-                    displayImage(fireUpImage2, sx, sy-range*32);
-                for(int i = 1; i<range; i++)
-                    if((y-i)>=0 && map.get(y-i,x)==FIRE)
-                        displayImage(fireUpMiddleImage2, sx, sy-i*32);
-                if(x+range<13 && map.get(y,x+range)==FIRE)
-                    displayImage(fireRightImage2, sx+range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x+i)<13 && map.get(y,x+i)==FIRE)
-                        displayImage(fireRightMiddleImage2, sx+i*32, sy);
-                if(y+range<11 && map.get(y+range,x)==FIRE)
-                    displayImage(fireDownImage2, sx, sy+range*32);
-                for(int i = 1; i<range; i++)
-                    if((y+i)<11 && map.get(y+i,x)==FIRE)
-                        displayImage(fireDownMiddleImage2, sx, sy+i*32);
-            }
-            break;
-        case 7:
-            if(state==EXPLODE){
-                 displayImage(fireCenterImage1, sx, sy);
-                if(x-range>=0 && map.get(y,x-range)==FIRE)
-                    displayImage(fireLeftImage1, sx-range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x-i)>=0 && map.get(y,x-i)==FIRE)
-                        displayImage(fireLeftMiddleImage1, sx-i*32, sy);
-                if(y-range>=0 && map.get(y-range,x)==FIRE)
-                    displayImage(fireUpImage1, sx, sy-range*32);
-                for(int i = 1; i<range; i++)
-                    if((y-i)>=0 && map.get(y-i,x)==FIRE)
-                        displayImage(fireUpMiddleImage1, sx, sy-i*32);
-                if(x+range<13 && map.get(y,x+range)==FIRE)
-                    displayImage(fireRightImage1, sx+range*32, sy);
-                for(int i = 1; i<range; i++)
-                    if((x+i)<13 && map.get(y,x+i)==FIRE)
-                        displayImage(fireRightMiddleImage1, sx+i*32, sy);
-                if(y+range<11 && map.get(y+range,x)==FIRE)
-                    displayImage(fireDownImage1, sx, sy+range*32);
-                for(int i = 1; i<range; i++)
-                    if((y+i)<11 && map.get(y+i,x)==FIRE)
-                        displayImage(fireDownMiddleImage1, sx, sy+i*32);
-            }
-            break;
-    }*/
+
 }
